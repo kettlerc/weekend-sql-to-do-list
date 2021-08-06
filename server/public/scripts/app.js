adaefa73476f687edce2fcfb22667f208b48463d
Curtis Kettler <curtiskettler@gmail.com>
2021-08-06 08:53:11 -0500

    Client GET written

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -4,10 +4,25 @@ $(document).ready(onReady);
 function onReady() {
     console.log('JQ');
     $('#newTaskForm').on('click', '#addBtn', addTask)
+    getTasks();
 };
 
 //GET
-
+function getTasks() {
+    $("#taskList").empty();
+    $.ajax({
+        type: 'GET',
+        url: '/taskmanager'
+    }).then((response) => {
+        for (let i=0; i<response.length; i++) {
+            $("taskList").append(`
+                <li>${response[i].task}</li>
+            `);
+        }
+    }).catch((err) => {
+        console.log('Can\'t get tasks...');
+    });
+}
 
 
 //POST
@@ -23,7 +38,8 @@ function addTask() {
         url: '/taskmanager',
         data: objectToSend
     }).then((response) => {
-        $('#newTask').val('')
+        $('#newTask').val(''),
+        getTasks();
     }).catch((err) => {
         console.log('POST err', err);   
     });
